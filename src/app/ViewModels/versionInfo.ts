5e9c5cb3a48d3943b01b3e932a2815c09492da00
aditimarathe <aditimarathe1@gmail.com>
2018-03-21 07:30:00 +0530

    added some real cool interfaces

--- src/app/ViewModels/versionInfo.ts
+++ src/app/ViewModels/versionInfo.ts
@@ -3,3 +3,56 @@ export interface VersionInfo{
     CompanyName:string;
     Version:string;
 }
+
+export interface User{
+    username: string;
+    password:string;
+    errormessage:string;
+}
+
+export interface loggedinUser{
+  username:string;
+  fullname:string;
+  email:string;
+  loggedinTime:Date;
+  token:string;
+}
+
+export interface Book{
+    Bookid:string;
+    bookTitle:string;
+    bookDesc:string;
+    bookAuthers:string[];
+    bookpublished:Boolean;
+    bookFirstPublishedOn:Date;
+    Revision:number;
+    bookLastPublishedOn:Date;
+    BookPrice:number;
+    Chapters:BookChapters[];
+}
+
+export interface BookChapters{
+    chapterId:string;
+    bookId:string;
+    chapterNumber:number;
+    chapterTitle:string;
+    chapterContent:string;
+    chapterPrice:number;
+    isChapterPublished:Boolean;
+
+}
+
+
+export interface BoughtBook {
+    BoughtBookId:string;
+    Bookid:string;
+    ChaptersBought:string[];
+}
+
+
+
+export  interface Shelf{
+    ShelfId:string;
+    ShelfOwner:string;
+    BoughtBooks:BoughtBook[];
+}
